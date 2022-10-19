c35938d94ee92d532669ab6a390c015d2d758001
Lee-moran <leemoran06@hotmail.com>
2022-10-19 08:33:00 +0000

    updated Urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from posts import views
+from profiles import views
 
 urlpatterns = [
-    path('posts/', views.PostList.as_view()),
-    path('posts/<int:pk>/', views.PostDetail.as_view())
-    ]
-
+    path('profiles/', views.ProfileList.as_view()),
+    path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
+]
