41bba317de51d2ad797ec195792ec800f36f0bbd
Lee-moran <leemoran06@hotmail.com>
2022-10-19 12:33:05 +0000

    cors

--- profiles/views.py
+++ profiles/views.py
@@ -17,12 +17,10 @@ class ProfileList(generics.ListAPIView):
         following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
-    
     filter_backends = [
         filters.OrderingFilter,
         DjangoFilterBackend,
     ]
-
     filterset_fields = [
         'owner__following__followed__profile'
     ]
